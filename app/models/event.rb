3f9d07f8942d1da616ac2e2dd9ada37e48e8d936
StlMaris123 <stellamarisnjage@gmail.com>
2018-06-04 14:02:18 +0300

    implement custom validations using custom validation methods

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,21 @@
 class Event < ApplicationRecord
   validates :name, presence: {message: "usiache jina blank priss!!"}
   validates :description, presence: {message: "we want to know what the event is about"}
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+
+  validate :start_date_before_end_date
+  validate :start_date_cant_be_in_past
+
+  def start_date_cant_be_in_past
+    if start_date < Date.today
+      errors.add(:start_date, "event can be only in future")
+    end
+  end
+
+  def start_date_before_end_date
+    if start_date >  end_date
+      errors.add(:start_date, "tangu lini event ikaisha before ianze")
+    end
+  end
 end
