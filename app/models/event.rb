09fe97d3eb2289ce6a6cce352d18e05c29906213
StlMaris123 <stellamarisnjage@gmail.com>
2018-06-04 13:58:10 +0300

    custom validation messages

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,4 @@
 class Event < ApplicationRecord
+  validates :name, presence: {message: "usiache jina blank priss!!"}
+  validates :description, presence: {message: "we want to know what the event is about"}
 end
